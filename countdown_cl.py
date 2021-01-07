bb69d9d898b79c5f703b27fde6c2e7efff819ec6
Max Bergmark <max.bergmark@gmail.com>
2021-01-07 13:45:00 +0100

    Refactoring

--- countdown_cl.py
+++ countdown_cl.py
@@ -1,8 +1,9 @@
 import pyopencl as cl
 import numpy as np
-from itertools import combinations
+from itertools import combinations, combinations_with_replacement
 from time import perf_counter as clock
 from collections import defaultdict
+from math import factorial
 import sys
 
 NUM_NUMBERS = 6
@@ -16,11 +17,7 @@ DIVISION_FAIL_INDEX = 1001
 PERMUTATION_FAIL_INDEX = 1002
 PERMUTATION_SUCCESS_INDEX = 1003
 
-def fac(n):
-	p = 1
-	for i in range(1, n+1):
-		p *= i
-	return p
+KERNEL_NAME = "countdown_kernel.cl"
 
 def time_function(f):
 	def wrapped(*args, **kw):
@@ -43,11 +40,12 @@ class DataSet:
 		self.expressions_np = np.array(expressions, dtype=np.int32)
 		self.n = len(expressions)
 		self.total_dataset_perms = self.num_perms * self.n
+		self.extra_stats = defaultdict(int)
+
 		DataSet.total_perms += self.total_dataset_perms
 		DataSet.num_batches += 1
 		self.setup_buffers(ctx)
 
-	# @time_function
 	def setup_buffers(self, ctx):
 		mf = cl.mem_flags
 
@@ -64,28 +62,23 @@ class DataSet:
 			mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=self.dims_np)
 
 	def start_kernel(self, prg, queue):
-		print(self.total_dataset_perms, self.total_perms)
 		current_part = 100 * self.total_dataset_perms / self.total_perms
 		print(f"Running batch {self.idx+1:2d}/{DataSet.num_batches:2d}:", 
 			f"({current_part:7.3f}%)", 
 			f"{self.n:6d} items, {self.num_perms:8d} permutations")
-		t0 = clock()
+
 		cl.enqueue_copy(queue, self.expressions_g, self.expressions_np)
 		cl.enqueue_copy(queue, self.dims_g, self.dims_np)
 		self.event = prg.evaluate(queue, (self.n,), None, 
 			self.expressions_g, self.result_g, self.dims_g)
-		t1 = clock()
-		self.total_elapsed = t1 - t0
 
 	def await_kernel(self, queue):
 		self.event.wait()
-		print(self.total_dataset_perms, self.total_perms)
-		elapsed = 1e-9*(self.event.profile.end - self.event.profile.start)
+		self.kernel_time = 1e-9*(self.event.profile.end - self.event.profile.start)
 		self.copy_event = cl.enqueue_copy(queue, self.result_np, self.result_g)
 
-		print(f"Elapsed: {elapsed:7.3f}s / {self.total_elapsed:7.3f}s,", end="\t")
+		print(f"Elapsed: {self.kernel_time:7.3f}s,", end="\t")
 		print(f"Done with {100 * self.num_perms / self.total_perms:6.2f}%\n")
-		return elapsed
 
 	def collect_data(self, output_dict):
 		self.copy_event.wait()
@@ -93,6 +86,7 @@ class DataSet:
 			numbers = tuple(self.expressions_np[i,-NUM_NUMBERS:])
 			counts = self.result_np[i,:MAX_TARGET]
 			output_dict[numbers] += counts
+			self.update_extra_stats(i)
 
 
 	def update_extra_stats(self, i):
@@ -112,64 +106,45 @@ class CountdownGame:
 	def __init__(self):
 		self._operators = None
 		self.total_kernel_time = 0
-		self.kernel_filename = "countdown_kernel.cl"
 		if len(sys.argv) == 2:
 			self.output_filename = sys.argv[1]
 		else:
 			self.output_filename = "/tmp/output.csv"
 		print(f"Output filename: {self.output_filename}")
-		self.ctx = cl.create_some_context()
-		self.queue = cl.CommandQueue(self.ctx, 
-			properties=cl.command_queue_properties.PROFILING_ENABLE)
+		self.setup_opencl()
+		self.make_kernel()
 		self.output_dict = defaultdict(
 			lambda: np.zeros((MAX_TARGET,), dtype=np.int32))
 		self.extra_stats = defaultdict(int)
 		
 		self.generate_data_sets()
-		self.setup_opencl()
-		self.make_kernel()
-
-
-	@time_function
-	def setup_opencl(self):
-		mf = cl.mem_flags
-		self.dims_np = np.array(list(self.data_np.shape) + [0], dtype=np.int32)
-		self.result_np = np.zeros(
-			(self.dims_np[0], MAX_TARGET + NUM_EXTRA_VALUES), dtype=np.int32)
 		self.output_np = np.zeros(
 			(len(self.numbers), NUM_NUMBERS + MAX_TARGET), dtype=np.int32)
 
-		self.data_g = cl.Buffer(self.ctx, 
-			mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=self.data_np)
-		self.result_g = cl.Buffer(self.ctx, 
-			mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=self.result_np)
-		self.dims_g = cl.Buffer(self.ctx, 
-			mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=self.dims_np)
+	@time_function
+	def setup_opencl(self):
+		self.ctx = cl.create_some_context()
+		self.queue = cl.CommandQueue(self.ctx, 
+			properties=cl.command_queue_properties.PROFILING_ENABLE)
 
 	@time_function
 	def make_kernel(self):
-		kernel = open(self.kernel_filename, "r").read()
-		self.prg = cl.Program(self.ctx, kernel).build(["-cl-fast-relaxed-math"])
+		kernel = open(KERNEL_NAME, "r").read()
+		self.prg = cl.Program(self.ctx, kernel).build()
 
 
 	@property
 	def operators(self):
 		if self._operators is None:
-			self._operators = [
-				"+++++", "++++-", "++++*", "++++/", "+++--", "+++-*", "+++-/", 
-				"+++**", "+++*/", "+++//", "++---", "++--*", "++--/", "++-**", 
-				"++-*/", "++-//", "++***", "++**/", "++*//", "++///", "+----", 
-				"+---*", "+---/", "+--**", "+--*/", "+--//", "+-***", "+-**/", 
-				"+-*//", "+-///", "+****", "+***/", "+**//", "+*///", "+////", 
-				"-----", "----*", "----/", "---**", "---*/", "---//", "--***", 
-				"--**/", "--*//", "--///", "-****", "-***/", "-**//", "-*///", 
-				"-////", "*****", "****/", "***//", "**///", "*////", "/////"
-			]
+			self._operators = list(map(
+				"".join, 
+				combinations_with_replacement("+-*/", NUM_SYMBOLS)
+			))
 		return self._operators
 
 	def map_operators(self, operators):
 		return tuple(map(
-			lambda o: " +-*/".index(o)*-1, 
+			lambda o: ("+-*/".index(o) + 1) * (-1), 
 			operators
 		))
 
@@ -181,11 +156,11 @@ class CountdownGame:
 			combinations(big_numbers + small_numbers, NUM_NUMBERS)))
 		return sorted(choices)
 
-	def calculate_perms(self, data_set):
-		perms = fac(len(data_set))
-		token_set = set(data_set)
+	def calculate_perms(self, expression):
+		perms = factorial(NUM_TOKENS)
+		token_set = set(expression)
 		for token in token_set:
-			perms //= fac(data_set.count(token))
+			perms //= factorial(expression.count(token))
 		return perms
 
 	@time_function
@@ -195,9 +170,9 @@ class CountdownGame:
 		self.numbers = self.get_numbers()
 		for n in self.numbers:
 			for o in mapped_operators:
-				data_set = sorted(o) + list(n)
-				perms = self.calculate_perms(data_set)
-				data[perms].append(data_set)
+				expression = sorted(o) + list(n)
+				perms = self.calculate_perms(expression)
+				data[perms].append(expression)
 
 		self.np_data = defaultdict(np.ndarray)
 		self.data_sets = []
@@ -212,8 +187,8 @@ class CountdownGame:
 
 	def run_all_data_sets(self):
 		print()
+		t0 = clock()
 		for data_set in self.data_sets:
-			# data_set.total_perms = self.total_perms
 			data_set.start_kernel(self.prg, self.queue)
 			# break
 
@@ -223,7 +198,12 @@ class CountdownGame:
 
 		for data_set in self.data_sets:
 			data_set.collect_data(self.output_dict)
+			for k, v in data_set.extra_stats.items():
+				self.extra_stats[k] += v
+			self.total_kernel_time += data_set.kernel_time
 			# break
+		t1 = clock()
+		self.total_elapsed = t1 - t0
 
 	def print_extra_stats(self):
 		for key, value in self.extra_stats.items():
